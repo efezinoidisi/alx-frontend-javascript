4e5ce7f40ef6484979998e9ef4b40bc066751111
efezino <enduranceidisi13@gmailcom>
2023-06-27 23:09:53 +0100

    second attempt

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,12 +1,8 @@
 export default function handleResponseFromAPI(promise) {
-  return new Promise((resolve, reject) => {
-    promise.then(() => {
+  return promise
+    .then(() => {
       console.log('Got a response from the API');
-      resolve({ status: 200, body: 'Success' });
-    });
-    promise.catch(() => {
-      console.log('Got a response from the API');
-      reject(new Error());
-    });
-  });
+      return { status: 200, body: 'Success' };
+    })
+    .catch(() => new Error());
 }

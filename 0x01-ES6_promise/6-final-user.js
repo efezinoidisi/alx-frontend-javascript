5b4c3ce7432c427962cb22af8c318330dc87bddb
efezino <enduranceidisi13@gmailcom>
2023-06-28 16:10:56 +0100

    remove console log

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -5,5 +5,5 @@ export default function handleProfileSignup(firstName, lastName, filename) {
   return Promise.allSettled([
     signUpUser(firstName, lastName),
     uploadPhoto(filename),
-  ]).then((results) => console.log(results));
+  ]).then((results) => results);
 }

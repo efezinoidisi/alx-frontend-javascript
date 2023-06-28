fab029ba623fbee3e58ff2a47b2f0d0e326ef806
efezino <enduranceidisi13@gmailcom>
2023-06-28 01:42:07 +0100

    use try..catch block

--- 0x01-ES6_promise/100-await.js
+++ 0x01-ES6_promise/100-await.js
@@ -1,11 +1,19 @@
 import { uploadPhoto, createUser } from './utils';
 
-export default async function asyncUploader() {
-  const photo = await uploadPhoto();
-  const user = await createUser();
+export default async function asyncUploadUser() {
+  try {
+    const photo = await uploadPhoto();
+    const user = await createUser();
 
-  return {
-    photo,
-    user,
-  };
+    return {
+      photo,
+      user,
+    };
+  } catch (err) {
+    console.log(err);
+    return {
+      photo: null,
+      user: null,
+    };
+  }
 }

07544ff03b83e0600bab437d9cbf4fad24d0c45c
efezino <enduranceidisi13@gmailcom>
2023-06-30 17:12:00 +0100

    attempt task 4

---
+++ 0x02-ES6_classes/4-pricing.js
@@ -0,0 +1,40 @@
+import Currency from './3-currency';
+
+export default class Pricing {
+  constructor(amount, currency) {
+    this._amount = amount;
+    this._currency = currency;
+  }
+
+  get amount() {
+    return this._amount;
+  }
+
+  set amount(value) {
+    if (typeof value !== 'number') {
+      throw new TypeError('Amount must be a number');
+    } else {
+      this._amount = value;
+    }
+  }
+
+  get currency() {
+    return this._currency;
+  }
+
+  set currency(value) {
+    if (value instanceof Currency) {
+      this._currency = value;
+    } else {
+      throw new TypeError('current must be a Currency object');
+    }
+  }
+
+  displayFullPrice() {
+    return `${this._amount} ${this._currency._name} (${this._currency._code})`;
+  }
+
+  static convertPrice(amount, conversionRate) {
+    return amount * conversionRate;
+  }
+}
